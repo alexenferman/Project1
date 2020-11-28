3afc55f24859bd87234ee2777c3d8e06bc396225
alexenferman <alextud0132@gmail.com>
2020-11-27 20:07:32 -0500

    update

--- files/msg.js
+++ files/msg.js
@@ -3,7 +3,14 @@ var x = document.getElementById("MsgSgj6");
 	x.style.display = "block";
 }
 
+function Msgaemsttrigger() {
+var x = document.getElementById("Msgaemst");
+	x.style.display = "block";
+}
+
 function closewindow() {
 var x = document.getElementById("MsgSgj6");
 	x.style.display = "none";
+var b = document.getElementById("Msgaemst");
+	b.style.display = "none";	
 }
